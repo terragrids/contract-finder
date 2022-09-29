b0e3a9e7452e67bbb81a82f0a84bfceda2284f86
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-09-29 14:32:53 +0100

    Add reach support

--- src/app.js
+++ src/app.js
@@ -5,19 +5,37 @@ import Koa from 'koa'
 import Router from '@koa/router'
 import errorHandler from './middleware/error-handler.js'
 import requestLogger from './middleware/request-logger.js'
+import { loadStdlib } from '@reach-sh/stdlib'
 
 dotenv.config()
 export const app = new Koa()
 const router = new Router()
 
 router.get('/', ctx => {
-    ctx.body = 'terragrids project contractor api'
+    ctx.body = 'terragrids project contract api'
 })
 
 router.get('/hc', async ctx => {
+    const stdlib = loadStdlib({
+        ...process.env,
+        REACH_CONNECTOR_MODE: 'ALGO'
+    })
+
+    const env = process.env.ENV === 'prod' ? 'MainNet' : 'TestNet'
+
+    stdlib.setProviderByName(env)
+    const provider = await stdlib.getProvider()
+
+    const [algoClientHC, algoIndexerHC] = await Promise.all([provider.algodClient.healthCheck().do(), provider.indexer.makeHealthCheck().do()])
+
     ctx.body = {
         env: process.env.ENV,
-        region: process.env.AWS_REGION
+        region: process.env.AWS_REGION,
+        reach: {
+            network: env,
+            algoClient: JSON.stringify(algoClientHC) === '{}' ? 'ok' : 'error',
+            algoIndexer: algoIndexerHC.version ? 'ok' : 'error'
+        }
     }
 })
 
