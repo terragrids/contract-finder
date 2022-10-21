e8775650a2e028468bed297781475ddf6a873e55
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-10-21 10:05:30 +0100

    Change project db attributes

--- src/repository/project.repository.js
+++ src/repository/project.repository.js
@@ -4,16 +4,17 @@ import DynamoDbRepository from './dynamodb.repository.js'
 
 export default class ProjectRepository extends DynamoDbRepository {
     projectPrefix = 'project'
-    creatorPrefix = 'creator'
-    itemLogName = 'project'
+    userPrefix = 'user'
+    itemName = 'project'
 
     async createProject(contractId, creator) {
         return await this.put({
             item: {
                 pk: { S: `${this.projectPrefix}|${contractId}` },
-                creator: { S: `${this.creatorPrefix}|${creator}` }
+                gsi1pk: { S: `${this.userPrefix}|${creator}` },
+                gsi2pk: { S: `type|${this.itemName}` }
             },
-            itemLogName: this.itemLogName
+            itemLogName: this.itemName
         })
     }
 
@@ -21,13 +22,13 @@ export default class ProjectRepository extends DynamoDbRepository {
         try {
             const data = await this.get({
                 key: { pk: { S: `${this.projectPrefix}|${contractId}` } },
-                itemLogName: this.itemLogName
+                itemLogName: this.itemName
             })
 
             return data.Item
                 ? {
                       id: contractId,
-                      creator: data.Item.creator.S.replace(`${this.creatorPrefix}|`, '')
+                      creator: data.Item.gsi1pk.S.replace(`${this.userPrefix}|`, '')
                   }
                 : null
         } catch (e) {
